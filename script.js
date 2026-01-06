2e51f227466cbd807a302eddd35fe958e9ec240f
meducatif22-dotcom <meducatif22@gmail.com>
2026-01-06 00:04:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const { data, error } = await supabase
+  .from("client")
+  .insert([
+    { nom, mail, adresse, message }
+  ])
+  .select();
 
-const supabaseUrl = "https://uxartdzzansqlvvbcaxc.supabase.co";
-const supabaseKey = "TA_CLE_PUBLIQUE";
-const supabase = createClient(supabaseUrl, supabaseKey);
+console.log("DATA :", data);
+console.log("ERROR :", error);
 
-const form = document.getElementById("clientForm");
+if (error) {
+  alert("Erreur Supabase : " + error.message);
+  return;
+}
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault(); // empêche le rechargement de la page
-
-  const nom = document.getElementById("nom").value;
-  const mail = document.getElementById("mail").value;
-  const adresse = document.getElementById("adresse").value;
-  const message = document.getElementById("message").value;
-
-  const { error } = await supabase
-    .from("client")
-    .insert([
-      { nom, mail, adresse, message }
-    ]);
-
-  if (error) {
-    console.error("Erreur Supabase :", error.message);
-    alert("Erreur lors de l'envoi");
-  } else {
-    // ✅ Redirection si succès
-    window.location.href = "page2.html";
-  }
-});
+// ✅ Redirection seulement si succès
+window.location.href = "page2.html";
